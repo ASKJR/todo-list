cb68aef0d82a1e66c46dc3aacd6484c2a4d8b8cf
ASKJR <albertokatojr@gmail.com>
2020-07-19 22:08:31 -0300

    input component added

---
+++ todo-vue/src/components/Input.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-col cols="12">
+    <div class="input-box">
+      <div class="input-task">
+        <v-text-field label="Type a new task"></v-text-field>
+      </div>
+      <div class="input-btn">
+        <v-btn small color="primary" depressed>ADD</v-btn>
+      </div>
+    </div>
+  </v-col>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped>
+.input-box {
+  display: inline-flex;
+}
+.input-task {
+  margin: 5px;
+}
+.input-btn {
+  margin-bottom: 20px;
+  align-self: center;
+}
+.input-btn button {
+  min-width: 61px;
+}
+</style>
--- todo-vue/src/components/Todo.vue
+++ todo-vue/src/components/Todo.vue
@@ -3,6 +3,7 @@
     <v-row class="text-center pt-5">
       <app-title title="Vue To-Do list"></app-title>
       <app-logo></app-logo>
+      <app-input></app-input>
     </v-row>
   </v-container>
 </template>
@@ -10,11 +11,13 @@
 <script>
 import Logo from './Logo.vue';
 import Title from './Title.vue';
+import Input from './Input.vue';
 
 export default {
   components: {
     appLogo: Logo,
     appTitle: Title,
+    appInput: Input,
   },
 };
 </script>
